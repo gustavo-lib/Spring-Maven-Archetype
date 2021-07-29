3c359cff9e201aed09cbda644ba611361c617794
gus <ggortiz09@gmail.com>
2021-07-29 19:01:38 -0300

    first commit

---
+++ src/main/java/com/gortiz/maven_achetype/App.java
@@ -0,0 +1,13 @@
+package com.gortiz.maven_achetype;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
